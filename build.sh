3c8e2eb8345c4397f796d43c5a7e37af67fbfdb3
knizmi <knizek.milan@gmail.com>
2015-03-15 21:40:26 +0100

    create build script

---
+++ build.sh
@@ -0,0 +1,20 @@
+echo -e "Making moto g (falcon) zImage\n"
+export PATH=$PATH:~/toolchains/linaro4.9_Cortex-A7/bin
+export ARCH=arm
+export SUBARCH=arm
+export CROSS_COMPILE=arm-eabi-
+
+make msm8226_defconfig
+
+make -j4
+
+# modules
+find ./ -type f -name '*.ko' -exec cp -f {} ../zip/system/lib/modules/ \;
+mv ../zip/system/lib/modules/wlan.ko ../zip/system/lib/modules/pronto/pronto_wlan.ko
+
+# copy zImage
+cp -f arch/arm/boot/zImage-dtb ../zip/kernel/
+ls -l ../zip/kernel/zImage-dtb
+cd ../zip
+zip -r -9 moto_G.zip * > /dev/null
+mv moto_G.zip ../
